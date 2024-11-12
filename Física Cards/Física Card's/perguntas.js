b15cce97500bd3349dd8192c0e15d7746499ddfb
rikkaah <gmangle98@gmail.com>
2024-11-12 12:18:21 -0300

    Add files via upload

---
+++ Física Cards/Física Card's/perguntas.js
@@ -0,0 +1,37 @@
+criaCartao(
+    'Termodinâmica.',
+    'Qual é a Primeira e Segunda lei da Termodinâmica?',
+    ' a Primeira lei da termodinâmica nos fala sobre a conservação de energia entre processos, enquanto a Segunda lei da termodinâmica nos fala sobre a direcionalidade dos processos, ou seja, da menor para a maior entropia.'
+
+)
+
+criaCartao(
+    'Termodinâmica',
+    'Qual é o nome apropriado para o calor cedido pela esfera de metal ao gelo?',
+    'Esse calor cedido pela esfera é chamado de calor sensível, pois ele provoca uma variação de temperatura na esfera, fazendo com que ela esfrie.'
+)
+
+criaCartao(
+    'Termodinâmica',
+    'Qual é o nome para o calor recebido pelo gelo?',
+    'o calor recebido pelo gelo é utilizado para a mudança de estado físico, ou seja, para derreter o gelo. Portanto, é calor latente de fusão.'
+)
+
+criaCartao(
+    'Termodinâmica',
+    'O que é o ponto triplo da água?',
+    'O ponto triplo da água, bem como o de qualquer outra substância, corresponde a um ponto, no diagrama de fases, em que os estados sólido, líquido e gasoso coexistem em equilíbrio termodinâmico.'
+)
+
+criaCartao(
+    'Termometria',
+    'O que é Condutividade Térmica?',
+    'A condutividade térmica é a capacidade de um material de transferir calor. Materiais com alta condutividade, como metais, transferem calor rapidamente, enquanto materiais com baixa condutividade, como isopor, agem como isolantes'
+)
+
+criaCartao(
+    'Calorimetria',
+    'Onde aplicar a Calorimetria?',
+    'Na física, é aplicada em estudos de transições de fase e propriedades térmicas de materiais. Em farmacologia e biotecnologia, serve para analisar a estabilidade térmica de fármacos e interações biomoleculares.'
+
+)
